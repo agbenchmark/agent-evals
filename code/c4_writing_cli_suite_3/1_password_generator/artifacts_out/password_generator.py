b1980d96c897fe10901384864b361d2058322588
Merwane Hamadi <merwanehamadi@gmail.com>
2023-08-05 08:39:44 -0700

    Add more coding challenges
    Signed-off-by: Merwane Hamadi <merwanehamadi@gmail.com>

---
+++ code/c4_writing_cli_suite_3/1_password_generator/artifacts_out/password_generator.py
@@ -0,0 +1,15 @@
+import string
+import random
+
+def generate_password(length: int) -> str:
+    if length < 8 or length > 16:
+        raise ValueError("Password length must be between 8 and 16 characters.")
+
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+
+    return password
+
+if __name__ == "__main__":
+    password_length = random.randint(8, 16)
+    print(generate_password(password_length))
