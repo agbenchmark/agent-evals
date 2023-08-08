502590b6214d45c6a8a58e845750e1a46fc1bf20
Merwane Hamadi <merwanehamadi@gmail.com>
2023-08-08 12:55:01 -0700

    Update password challenge

--- code/c4_writing_cli_suite_3/1_password_generator/artifacts_out/password_generator.py
+++ code/c4_writing_cli_suite_3/1_password_generator/artifacts_out/password_generator.py
@@ -7,9 +7,15 @@ def generate_password(length: int) -> str:
         raise ValueError("Password length must be between 8 and 16 characters.")
 
     characters = string.ascii_letters + string.digits + string.punctuation
-    password = "".join(random.choice(characters) for _ in range(length))
-
-    return password
+    password = [
+        random.choice(string.ascii_lowercase),
+        random.choice(string.ascii_uppercase),
+        random.choice(string.digits),
+        random.choice(string.punctuation),
+    ]
+    password += [random.choice(characters) for _ in range(length - 4)]
+    random.shuffle(password)
+    return "".join(password)
 
 
 if __name__ == "__main__":
