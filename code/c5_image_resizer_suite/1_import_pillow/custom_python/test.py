a747df27e981b120b1ee0e3561266f7b586b7970
Merwane Hamadi <merwanehamadi@gmail.com>
2023-08-08 09:43:33 -0700

    Add web app creation challenge

--- code/c5_image_resizer_suite/1_import_pillow/custom_python/test.py
+++
@@ -1,5 +0,0 @@
-import art
-
-ascii_art = art.text2art("Hello!")
-with open("ascii_art.txt", "w") as f:
-    f.write(ascii_art)
--- interface/agent_protocol_suite/1_create_agent_task/custom_python/test.py
+++ interface/agent_protocol_suite/1_create_agent_task/custom_python/test.py
@@ -1,13 +1,16 @@
-# mypy: ignore-errors
-
 import subprocess
+import sys
 
 
 def call_agent_protocol() -> None:
     command = (
         "agent-protocol test --url=http://127.0.0.1:8000 -k test_create_agent_task"
     )
-    subprocess.run(command, shell=True)
+    try:
+        result = subprocess.run(command, shell=True, check=True)
+        sys.exit(result.returncode)
+    except subprocess.CalledProcessError as e:
+        sys.exit(e.returncode)
 
 
 if __name__ == "__main__":
